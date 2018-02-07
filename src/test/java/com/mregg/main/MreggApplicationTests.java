22ac6e8c363096ef01472e1175b1bb482dee5be1
e <rodrigo@localhost>
2018-02-07 18:30:28 -0300

    test

---
+++ src/test/java/com/mregg/main/MreggApplicationTests.java
@@ -0,0 +1,23 @@
+package com.mregg.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+/*RunWith(SpringRunner.class)
+SpringBootTest*/
+public class MreggApplicationTests {
+
+	//Test
+	public void contextLoads() {
+		 
+	}
+
+}
