6078eef9866eff80ccb88631f5d00fc6adf7e5fb
e <rodrigo@localhost>
2018-02-08 17:38:26 -0300

    For configure load in test unit

---
+++ src/main/java/com/mregg/main/Configure.java
@@ -0,0 +1,18 @@
+package com.mregg.main;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.mregg.main.jersey.Config;
+
+@Configuration
+@ComponentScan
+
+
+public class Configure {
+	
+
+}
