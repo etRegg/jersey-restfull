5f6d1c94045437b8a6d727ae3b2f3e3a1c9081b8
e <rodrigo@localhost>
2018-02-08 20:20:16 -0300

    bit test for load context

--- src/test/java/com/mregg/main/MreggApplicationTests.java
+++ src/test/java/com/mregg/main/MreggApplicationTests.java
@@ -2,20 +2,23 @@ package com.mregg.main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-/*RunWith(SpringRunner.class)
-SpringBootTest*/
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+//DataJpaTest
+
+@EnableAutoConfiguration
 public class MreggApplicationTests {
 
-	//Test
+	@Test
 	public void contextLoads() {
 		 
 	}
